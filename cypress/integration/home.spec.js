17d4a797f5e155cb1c355468cc6713eeab92365c
kahturin <karinaalveslima@icloud.com>
2022-02-14 21:49:29 -0300

    cypress básico qa-ninja

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,7 @@
 describe('home page', ()=>{
     it('app deve estar online', ()=>{
         cy.viewport(1920, 1080)
-        cy.visit('https://buger-eats.vercel.app')
+        cy.visit('/')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
 
         cy.get('a[href="/deliver"]').click()
